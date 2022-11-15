8054807d3eb19119b9016539f722e9b1c3812a02
Arindom Das <arindom@minigamelab.com>
2022-11-15 10:45:09 +0600

    first working actionable phase

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -6,26 +6,57 @@ using UnityEngine.Animations.Rigging;
 
 public class Player : MonoBehaviour
 {
-    private static Dictionary<string, int> ParameterDictionary = new Dictionary<string, int>();
+    private static Dictionary<int, int> ParameterDictionary;
+    private Dictionary<RigLayer, float> rigDefaultValueDictionary;
 
     [SerializeField]
     private Transform lifeBarTransform,
         ownHeadTargetPointerTransform,
         headTargetPointTransform;
+    [SerializeField] private Rig chestRig;
 
-    private Transform mainCamT;
+    private PlayerController playerController;
+    private Transform ownTransform, mainCamT;
     private Animator animator;
-    private RigBuilder rigbuilder;
-    private int lifeRemaining, totalLife = 100;
-    private bool isRigActive = false;
+    private RigBuilder rigBuilder;
+    private int lifeRemaining,
+        totalLife = 100,
+        lastFlag;
+    private bool __isRigActive = false,
+        movingToEnd = false;
+    private float movingDuration = 0.4f,
+        distanceBetweenCharacters;
+    private Vector3 initPos,
+        initRot,
+        opponentPos,
+        stepToPos,
+        rotationTo;
+
+    public bool IsDied => lifeRemaining <= 0;
+    private bool isRigActive
+    {
+        get => __isRigActive;
+        set
+        {
+            if (__isRigActive == value) return;
+            __isRigActive = value;
+            ApplyRigLayersActivationStatus();
+        }
+    }
 
     void Start()
     {
+        ownTransform = transform;
         mainCamT = Camera.main.transform;
         animator = GetComponent<Animator>();
-        rigbuilder = GetComponent<RigBuilder>();
+        rigBuilder = GetComponent<RigBuilder>();
+        playerController = PlayerController.Instance;
+        MapRigLayerDefaultValues();
         CreateParameterDictionary();
         SetLife(totalLife);
+
+        initPos = ownTransform.position;
+        initRot = ownTransform.eulerAngles;
     }
 
     private void Update()
@@ -34,22 +65,27 @@ public class Player : MonoBehaviour
         ownHeadTargetPointerTransform.position = headTargetPointTransform.position;
     }
 
+    private void MapRigLayerDefaultValues()
+    {
+        rigDefaultValueDictionary = new Dictionary<RigLayer, float>();
+        foreach (var layer in rigBuilder.layers)
+        {
+            rigDefaultValueDictionary[layer] = layer.rig.weight;
+        }
+    }
+
     private void CreateParameterDictionary()
     {
+        ParameterDictionary = new Dictionary<int, int>();
         var keys = Enum.GetNames(typeof(ActionKeywords));
         var values = Enum.GetValues(typeof(ActionKeywords));
         for (int i = 0; i < keys.Length; i++)
         {
-            ParameterDictionary[keys[i]] = (int)values.GetValue(i);
+            ParameterDictionary[keys[i].GetHashCode()] = (int)values.GetValue(i);
         }
     }
 
-    private bool GetFlagStatus(in string flagName, in int flagStatuses)
-    {
-        return ((ParameterDictionary[flagName] & flagStatuses) > 0);
-    }
-
-    public void SetAnimatorFlags(int flags)
+    private void SetAnimatorFlags(int flags)
     {
         bool flag;
         int len = animator.parameterCount;
@@ -57,7 +93,7 @@ public class Player : MonoBehaviour
         for (int i = 0; i < len; i++)
         {
             param = animator.GetParameter(i);
-            flag = GetFlagStatus(param.name, flags);
+            flag = (ParameterDictionary[param.GetHashCode()] & flags) > 0;
             if (param.type == AnimatorControllerParameterType.Trigger)
             {
                 if (flag) animator.SetTrigger(param.name);
@@ -69,22 +105,21 @@ public class Player : MonoBehaviour
         }
     }
 
-    public void AnimationBackOnFightIdle()
+    private void ApplyRigLayersActivationStatus()
     {
-        SetRigLayersActivationStatus(true);
-    }
-
-    public void DidHitOpponent()
-    {
-        PlayerController.Instance.OnCollisionBetweenPlayer(this);
-    }
-
-    public void GotHit(int flags)
-    {
-        SetRigLayersActivationStatus(false);
-        ChangeLife(-40);
-        if (lifeRemaining <= 0) flags |= (int)ActionKeywords.Died;
-        SetAnimatorFlags(flags);
+        bool active = isRigActive;
+        float duration = 0.5f;
+        float startVal = 0;
+        float endVal = 0;
+        foreach (var layer in rigBuilder.layers)
+        {
+            if (active) endVal = rigDefaultValueDictionary[layer];
+            else startVal = rigDefaultValueDictionary[layer];
+            DOTween.To((x) =>
+            {
+                layer.rig.weight = x;
+            }, startVal, endVal, duration);
+        }
     }
 
     public void SetLife(int life)
@@ -96,24 +131,201 @@ public class Player : MonoBehaviour
     {
         lifeRemaining += deltaLife;
         if (lifeRemaining < 0) lifeRemaining = 0;
-
         lifeBarTransform.DOScaleX(lifeRemaining / (float)totalLife, 0.2f);
     }
 
-    private void SetRigLayersActivationStatus(bool activate)
+    public void SetOpponentPos(Vector3 opponentPos)
     {
-        if (isRigActive == activate) return;
-        float startVal = activate ? 0 : 1;
-        float endVal = ((int)startVal + 1) % 2;
-        foreach (var layer in rigbuilder.layers)
-        {
-            DOTween.To((x) =>
-            {
-                layer.rig.weight = x;
-            }, startVal, endVal, 0.5f);
-        }
-        isRigActive = activate;
+        this.opponentPos = opponentPos;
+        distanceBetweenCharacters = Vector3.Distance(initPos, opponentPos);
     }
 
-    public bool IsDied() => lifeRemaining <= 0;
+    public void DidHitOpponent()
+    {
+        playerController.OnCollisionBetweenPlayer(this);
+    }
+
+    public void DoHit(int flags)
+    {
+        lastFlag = flags;
+        SetAnimatorFlags(flags);
+    }
+
+    public void GotHit(int flags)
+    {
+        print(name + "gotHit::toEnd: " + movingToEnd + ", isRigActive: " + isRigActive);
+        if (movingToEnd || !isRigActive) return;
+        lastFlag = flags;
+        isRigActive = false;
+        ChangeLife(-18);
+        if (lifeRemaining <= 0)
+        {
+            flags |= (int)ActionKeywords.Died;
+            DOTween.Sequence().AppendInterval(2).AppendCallback(() => playerController.OnAPlayerDied(this));
+        }
+        SetAnimatorFlags(flags);
+    }
+
+    public void MatchEnd(bool won)
+    {
+        isRigActive = false;
+        lifeBarTransform.parent.gameObject.SetActive(false);
+        if (won)
+        {
+            animator.SetBool(ActionKeywords.Victory.ToString(), true);
+        }
+    }
+
+    #region Animation Callbacks
+
+    public void AnimationBackOnFightIdle()
+    {
+        isRigActive = true;
+    }
+
+    /// <summary> delta is negative if it goes back </summary>
+    public void DisplaceAsReaction()
+    {
+        print("DisplaceAsReaction action");
+        float deltaRot = 0, deltaPos = 0, snapDuration = 0, duration = 0, intervalDuration = 0;
+        int flags = lastFlag;
+        Vector3 pos = initPos, rot = initRot;
+        Ease ease = Ease.Linear;
+        if (PlayerController.GetActionFlagStatus(flags, ActionKeywords.DoHitFromLeft))
+        {
+            deltaPos = -0.8f;
+            deltaRot = 0f;
+
+            duration = 1.5f;
+            snapDuration = 0.2f;
+            intervalDuration = 0.5f;
+            ease = Ease.OutExpo;
+        }
+        else
+        {
+            deltaPos = -1.5f;
+            deltaRot = 0f;
+
+            duration = 2.5f;
+            snapDuration = 0f;
+            intervalDuration = 0.5f;
+            ease = Ease.OutExpo;
+        }
+        pos = Vector3.MoveTowards(initPos, opponentPos, deltaPos);
+        rot = initRot + deltaRot * Vector3.up;
+
+        DOTween.Sequence().AppendInterval(snapDuration).
+            Append(ownTransform.DOMove(pos, duration).SetEase(ease)).
+            Join(ownTransform.DORotate(rot, duration)).
+            AppendInterval(intervalDuration).
+            Append(ownTransform.DOMove(initPos, duration).SetEase(ease)).
+            Join(ownTransform.DORotate(initRot, duration));
+    }
+
+    public void ForwardStart()
+    {
+        if (movingToEnd || PlayerController.GetActionFlagStatus(lastFlag, ActionKeywords.GotHit)) return;
+        print("forward start");
+        movingToEnd = true;
+        AdditionalActionOnAnimation(true, lastFlag);
+    }
+
+    public void BackEnd()
+    {
+        print("back end");
+        movingToEnd = false;
+        AdditionalActionOnAnimation(false, lastFlag);
+    }
+
+    #endregion
+
+    private void AdditionalActionOnAnimation(bool toEnd, int flags)
+    {
+        print("additional action " + toEnd + " " + flags);
+        bool changePos = false, changeRot = false;
+        float deltaRot = 0, deltaPos = 0, snapDuration = 0;
+        Vector3 pos = initPos, rot = initRot;
+        Ease ease = Ease.Linear;
+        if (!toEnd)
+        {
+            changePos = changeRot = true;
+            pos = initPos;
+            rot = initRot;
+            snapDuration = 0.15f;
+            ease = Ease.OutSine;
+        }
+        else if (!PlayerController.GetActionFlagStatus(flags, ActionKeywords.Died) &&
+            PlayerController.GetActionFlagStatus(flags, ActionKeywords.DoHit))
+        {
+            if (PlayerController.GetActionFlagStatus(flags, ActionKeywords.UpperCut)) //uppercut
+            {
+                deltaPos = 0.6f;
+                deltaRot = 10f;
+            }
+            else
+            {
+                deltaPos = 0.6f;
+                deltaRot = 10f;
+                if (PlayerController.GetActionFlagStatus(flags, ActionKeywords.DoHitByHand)) //hand
+                {
+                    if (PlayerController.GetActionFlagStatus(flags, ActionKeywords.DoHitFromLeft)) //left
+                    {
+                        deltaPos = 0.55f;
+                    }
+                    else //right
+                    {
+                        deltaRot = -60;
+
+                        float prevVal = chestRig.weight;
+                        float newVal = 1;
+                        DOTween.To((x) =>
+                        {
+                            chestRig.weight = x;
+                        }, prevVal, newVal, movingDuration).OnComplete(() =>
+                        {
+                            DOTween.To((x) =>
+                            {
+                                chestRig.weight = x;
+                            }, newVal, prevVal, movingDuration);
+                        });
+                    }
+                }
+                else //leg
+                {
+                    if (PlayerController.GetActionFlagStatus(flags, ActionKeywords.DoHitFromLeft)) //left
+                    {
+                        deltaPos = 0.4f;
+                        deltaRot = 30f;
+                    }
+                    else //right
+                    {
+                        deltaPos = 0.35f;
+                        deltaRot = 0f;
+                    }
+                }
+            }
+            changePos = changeRot = true;
+            pos = Vector3.Lerp(initPos, opponentPos, deltaPos);
+            rot = initRot + deltaRot * Vector3.up;
+            snapDuration = 0.09f;
+            ease = Ease.OutExpo;
+        }
+
+        if (changePos)
+        {
+            if (snapDuration > 0)
+            {
+                DOTween.Sequence().AppendInterval(snapDuration).Append(ownTransform.DOMove(pos, movingDuration).SetEase(ease));
+            }
+            else ownTransform.DOMove(pos, movingDuration).SetEase(ease);
+        }
+        if (changeRot)
+        {
+            if (snapDuration > 0)
+            {
+                DOTween.Sequence().AppendInterval(snapDuration).Append(ownTransform.DORotate(rot, movingDuration));
+            }
+            else ownTransform.DORotate(rot, movingDuration);
+        }
+    }
 }
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -8,21 +8,20 @@ public enum ActionKeywords
     GotHit = 1 << 2,
     DoHitByHand = 1 << 3,
     DoHitFromLeft = 1 << 4,
-    PushedBack = 1 << 5,
-    FallDown = 1 << 6,
-    UpperCut = 1 << 7,
-    KnockOut = 1 << 8,
-    Victory = 1 << 9,
-    ChangeFromIdle = 1 << 10
+    UpperCut = 1 << 5,
+    KnockOut = 1 << 6,
+    Victory = 1 << 7,
 }
 
 public class PlayerController : MonoBehaviour
 {
     public static PlayerController Instance { get; private set; } = null;
-    
+
     [SerializeField] private Player player, bot;
     [SerializeField] private Toggle tgl_Hand, tgl_Left, tgl_UpperCut;
     private int currentFlags;
+
+    public int CurrentFlags => currentFlags;
 
     private void Awake()
     {
@@ -35,6 +34,12 @@ public class PlayerController : MonoBehaviour
         Instance = this;
     }
 
+    private void Start()
+    {
+        player.SetOpponentPos(bot.transform.position);
+        bot.SetOpponentPos(player.transform.position);
+    }
+
     private void OnDestroy()
     {
         if (Instance == this) Instance = null;
@@ -43,7 +48,7 @@ public class PlayerController : MonoBehaviour
     public void DoHit()
     {
         GetFlags();
-        player.SetAnimatorFlags(currentFlags | (int)ActionKeywords.DoHit);
+        player.DoHit(currentFlags | (int)ActionKeywords.DoHit);
     }
 
     private void GetFlags()
@@ -54,8 +59,21 @@ public class PlayerController : MonoBehaviour
         if (tgl_UpperCut.isOn) currentFlags |= (int)ActionKeywords.UpperCut;
     }
 
-    public void OnCollisionBetweenPlayer(Player hitBy){
+
+    public static bool GetActionFlagStatus(in int flagStatuses, in ActionKeywords actionKeyword)
+    {
+        return (((int)actionKeyword & flagStatuses) > 0);
+    }
+
+    public void OnCollisionBetweenPlayer(Player hitBy)
+    {
         Player hitTo = hitBy == player ? bot : player;
         hitTo.GotHit(currentFlags | (int)ActionKeywords.GotHit);
+    }
+
+    public void OnAPlayerDied(Player diedPlayer){
+        Player winPlayer = diedPlayer == player ? bot : player;
+        diedPlayer.MatchEnd(false);
+        winPlayer.MatchEnd(true);
     }
 }
